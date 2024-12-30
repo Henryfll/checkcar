98147e69f45b0b84a301dadac30b386ddc3d6741
Henry <henry.2154@hotmail.com>
2024-12-30 10:40:17 -0500

    file netlify

--- src/app/pages/starter/services/chatgpt.service.ts
+++ src/app/pages/starter/services/chatgpt.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class ChatgptService {
 
-  private apiKey = environment.apiKey;
+  private apiKey = (window as any).config.apiKey;
 
   private client: OpenAI;
 
